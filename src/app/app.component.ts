a8f3057412dfc6bba880a32f380a04c2964e72f1
eccefiles <eccefiles@hotmail.com>
2024-12-19 14:11:37 -0800

    Intial Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import {MatDialogModule} from '@angular/material/dialog';
+//import { SwitcherComponent } from './components/switcher/switcher.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink,MatDialogModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'cmrRSV2';
